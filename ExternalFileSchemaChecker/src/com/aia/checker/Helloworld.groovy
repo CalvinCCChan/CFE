baa2385333e4eb271496e298d3eac42ddebc7e5e
Calvin Chan <Calvin-cc.Chen@aia.com>
2013-07-03 18:00:44 +0800

    Commit by Calvin

--- ExternalFileSchemaChecker/src/com/aia/checker/Helloworld.groovy
+++ ExternalFileSchemaChecker/src/com/aia/checker/Helloworld.groovy
@@ -4,4 +4,12 @@ class Helloworld {
 	def printHelloWorld(){
 		print 'Hello world'
 	}	
+	
+	def int repeatAdd(val=5){
+		def returnValue = 0
+		for(i in 1..val)
+			returnValue += i
+			
+		return returnValue
+	}
 }
--- ExternalFileSchemaChecker/test/com/aia/checker/HelloworldTest.groovy
+++ ExternalFileSchemaChecker/test/com/aia/checker/HelloworldTest.groovy
@@ -9,10 +9,30 @@ import com.sun.net.httpserver.Authenticator.Success;
 class HelloworldTest {
 
 	@Test
-	public void testPrintHelloWorld() {
+	void testPrintHelloWorld() {
 		def printHelloWorld = new Helloworld();
 		printHelloWorld.printHelloWorld();
 		assertTrue("success",true);
 	}
-
+	
+	@Test
+	void testRepeatAdd(){
+		def printHelloWorld = new Helloworld();
+		def result = printHelloWorld.repeatAdd(3);
+		assertEquals("Not the Same", 6,result);
+	}
+	
+	@Test
+	void testRepeatAdd_equal10(){
+		def printHelloWorld = new Helloworld();
+		def result = printHelloWorld.repeatAdd(4);
+		assertEquals("Not the Same", 10,result);
+	}
+	
+	@Test
+	void testRepeatAdd_equalDefault(){
+		def printHelloWorld = new Helloworld();
+		def result = printHelloWorld.repeatAdd();
+		assertEquals("Not the Same", 15,result);
+	}
 }
